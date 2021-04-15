29e4e88a9fbbb575418726fd600dd386149a6108
Pedro <pedro.lima3@estudante.ifms.edu.br>
2021-04-15 10:27:41 -0300

    segundo commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do{
+                string[] vetor = Console.ReadLine().Split(' ');
+
+                double n1 = double.Parse(vetor[0]);
+                double n2 = double.Parse(vetor[2]);
+                double result;
+
+                char op = char.Parse(vetor[1]);
+
+                if(op == '+'){
+                    result = n1 + n2;
+                    Console.WriteLine(result.ToString("F1", CultureInfo.InvariantCulture));
+                }
+                else if(op == '-'){
+                    result = n1 - n2;
+                    Console.WriteLine(result.ToString("F1", CultureInfo.InvariantCulture));
+                }
+                else if(op == 'x' || op == '*'){
+                    result = n1 * n2;
+                    Console.WriteLine(result.ToString("F1", CultureInfo.InvariantCulture));
+                }
+                else if(op == '/'){
+                    result = n1 / n2;
+                    Console.WriteLine(result.ToString("F1", CultureInfo.InvariantCulture));
+                }
+                else{
+                    Console.WriteLine("Operação inválida.");
+                }
+
+                                
+            }while(true);
+        }
+    }
+}
